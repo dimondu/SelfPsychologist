5fd395b66b26ddd0932799644d1e6a85ce5fcac8
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-10-10 14:18:02 +0300

    Init tableView

---
+++ SelfPsychologist/MainCell.swift
@@ -0,0 +1,12 @@
+//
+//  MainCell.swift
+//  SelfPsychologist
+//
+//  Created by Дмитрий Дуров on 10.10.2022.
+//
+
+import UIKit
+
+class MainCell: UITableViewCell {
+    
+}
--- SelfPsychologist/MainViewController.swift
+++ SelfPsychologist/MainViewController.swift
@@ -7,14 +7,53 @@
 
 import UIKit
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate {
+    
+    var tableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        self.navigationItem.title = "Psychologist"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        
+        setTableView()
+        view.addSubview(tableView)
+        
+    
     }
+    
+    func setTableView() {
+        tableView = UITableView(frame: view.bounds)
+        tableView.register(MainCell.self, forCellReuseIdentifier: "MainViewCell")
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+}
 
+// MARK: - Table view data source
 
+extension MainViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "MainViewCell", for: indexPath) as? MainCell else { return UITableViewCell() }
+        
+        var content = cell.defaultContentConfiguration()
+        
+        content.text = "HELLO \(indexPath)"
+        
+        cell.contentConfiguration = content
+        
+        
+        
+        return cell
+    }
+    
+    
 }
 
 
