5bedf93afdc5ff0d9b35e67e7ca5c96f05718054
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-10-10 12:07:35 +0300

    Deleted storyboard

--- SelfPsychologist/MainViewController.swift
+++ SelfPsychologist/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  SelfPsychologist
 //
 //  Created by Дмитрий Дуров on 10.10.2022.
@@ -7,13 +7,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
 }
+
 
