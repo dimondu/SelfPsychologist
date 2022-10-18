0ed29b922a6f3ffcf8092ee8fa5e82460993a5ef
Durov Dmitrii <durov-dmitrii@mail.ru>
2022-10-18 11:47:54 +0300

    Переделал реализацию MainViewController с кнопок на UITableView

--- SelfPsychologist/MainViewController.swift
+++ SelfPsychologist/MainViewController.swift
@@ -9,26 +9,69 @@ import UIKit
 
 class MainViewController: UIViewController {
     
+    // MARK: - Private methods
+    
+    private var tableView =  UITableView()
+    
     // MARK: - Override methods
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         setupNavigationBar()
+        configureTableView()
     }
     
     // MARK: - Private methods
     
     private func setupNavigationBar() {
         title = "Self-Psychologist"
+        navigationController?.navigationBar.prefersLargeTitles = true
         
         let navBarAppearance = UINavigationBarAppearance()
         navBarAppearance.backgroundColor = .systemMint
         navBarAppearance.titleTextAttributes = [.foregroundColor: UIColor.white]
+        navBarAppearance.largeTitleTextAttributes = [.foregroundColor: UIColor.white]
         
         navigationController?.navigationBar.scrollEdgeAppearance = navBarAppearance
+        navigationController?.navigationBar.standardAppearance = navBarAppearance
     }
     
-   
+    private func configureTableView() {
+        view.addSubview(tableView)
+        setTableViewDelegates()
+        tableView.rowHeight = 100
+        tableView.register(MainCell.self, forCellReuseIdentifier: "menuCell")
+        setTableViewConstraints()
+    }
+    
+    private func setTableViewDelegates() {
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func setTableViewConstraints() {
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+// MARK: - Table view data source an Table view delegate
+
+extension MainViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        5
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "menuCell", for: indexPath) as? MainCell else { return UITableViewCell() }
+        cell.textLabel?.text = "HELLO"
+        return cell
+    }
     
 }
